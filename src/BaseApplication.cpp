88c82363f3f211b4de01344cf26b467040149567
Kirtez <kirtez@gmail.com>
2015-01-30 11:35:03 +0100

    Removed Windows-specific stuff regarding main()

--- src/BaseApplication.cpp
+++ src/BaseApplication.cpp
@@ -249,20 +249,16 @@ bool TinyOgre::go(void)
 }
 
 
-#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-#define WIN32_LEAN_AND_MEAN
-#include "windows.h"
-#endif
+//#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+//#define WIN32_LEAN_AND_MEAN
+//#include "windows.h"
+//#endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-    INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
-#else
-    int main(int argc, char *argv[])
-#endif
+ int main(int argc, char *argv[])
     {
         // Create application object
         TinyOgre app;
@@ -270,12 +266,7 @@ extern "C" {
         try {
             app.go();
         } catch( Ogre::Exception& e ) {
-#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-            MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
-#else
-            std::cerr << "An exception has occured: " <<
-                e.getFullDescription().c_str() << std::endl;
-#endif
+			 std::cerr << "An exception has occured: " << e.getFullDescription().c_str() << std::endl;
         }
 
         return 0;

4887d9f29ba7381cdb20b8aa878a2c8e68c19cf6
Joel Severin <git@joelhemsida.se>
2015-02-17 12:35:25 +0100

    Separated ogre plugin file into windows and linux ones.

--- src/main.cpp
+++ src/main.cpp
@@ -75,12 +75,17 @@ extern "C" {
 #endif
 
 int main(int argc, char* argv[]){
+#ifdef PLATFORM_WINDOWS
 #ifdef _DEBUG
     std::string resourceConfigPath = "resources_d.cfg";
     std::string pluginConfigPath = "plugins_d.cfg";
 #else
     std::string resourceConfigPath = "resources.cfg";
     std::string pluginConfigPath = "plugins.cfg";
+#endif
+#else
+    std::string resourceConfigPath = "resources.cfg";
+    std::string pluginConfigPath = "plugins-linux.cfg";
 #endif
 
     Rally::Controller::MainController mainController;

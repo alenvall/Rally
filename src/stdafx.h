3e65b9f7c9f28f0bdc03262f107714b59dfa2d4e
Kirtez <kirtez@gmail.com>
2015-02-26 11:09:18 +0100

    Added precompiled headers to MSVC projects, decreasing compile time

---
+++ src/stdafx.h
@@ -0,0 +1,25 @@
+// stdafx.h
+ 
+// Windows
+#if _WIN32
+#include "WinSock2.h"
+#include "windows.h"
+#endif
+
+// Ogre
+#include <OgreException.h>
+#include <OgreRoot.h>
+#include <OgreConfigFile.h>
+#include <OgreCamera.h>
+#include <OgreViewport.h>
+#include <OgreSceneManager.h>
+#include <OgreRenderWindow.h>
+#include <OgreFrameListener.h>
+#include <OgreEntity.h>
+#include <OgreWindowEventUtilities.h>
+#include <OgreString.h>
+#include <OgreVector3.h>
+#include <OgreQuaternion.h>
+#include <OgreResourceGroupManager.h>
+#include <OISKeyboard.h>
+#include <OISMouse.h>

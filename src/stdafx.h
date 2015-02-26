357779dff76c812f0ac979697e1ce8939fc37540
Kirtez <kirtez@gmail.com>
2015-02-26 13:06:11 +0100

    Added Bullet headers to precompiled header, decreasing compile time

--- src/stdafx.h
+++ src/stdafx.h
@@ -7,6 +7,7 @@
 #endif
 
 // Ogre
+#include <Ogre.h>
 #include <OgreException.h>
 #include <OgreRoot.h>
 #include <OgreConfigFile.h>
@@ -21,6 +22,13 @@
 #include <OgreVector3.h>
 #include <OgreQuaternion.h>
 #include <OgreResourceGroupManager.h>
+#include <OgreManualObject.h>
+#include <Terrain/OgreTerrain.h>
+#include <Terrain/OgreTerrainGroup.h>
+#include <Terrain/OgreTerrainMaterialGeneratorA.h>
 #include <OISKeyboard.h>
 #include <OISMouse.h>
 
+// Bullet
+#include <btBulletDynamicsCommon.h>
+#include <../Extras/Serialize/BulletWorldImporter/btBulletWorldImporter.h>

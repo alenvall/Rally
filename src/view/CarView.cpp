e6676fb6de0c1ac5f63af932fd8b245c132f380a
Joel Severin <git@joelhemsida.se>
2015-03-08 14:10:53 +0100

    Using the red car as standard, better for bloom testing.

--- src/view/CarView.cpp
+++ src/view/CarView.cpp
@@ -47,7 +47,7 @@ namespace Rally { namespace View {
         this->sceneManager = sceneManager;
 
         carEntity = sceneManager->createEntity(carName, "car.mesh");
-		carEntity->getSubEntity(4)->setMaterialName("carcolourhotpink");
+		carEntity->getSubEntity(4)->setMaterialName("carcolourred");
         carNode = sceneManager->getRootSceneNode()->createChildSceneNode();
         carNode->attachObject(carEntity);
 

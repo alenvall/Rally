91b1ef3b8275a07709f8a1673e1f693854854e6a
Victor Sandell <vicsand@student.chalmers.se>
2015-03-25 14:06:10 +0100

    Removed comments

--- src/model/World.cpp
+++ src/model/World.cpp
@@ -20,8 +20,8 @@ namespace Rally { namespace Model {
         playerCar.attachToWorld();
         // "Rally/src/model/Checkpoint.h|18|note:   no known conversion for argument 1 from ‘btVector3’ to ‘btVector3&’|
         // TODO: Fix compile error.
-		//finish.attachToWorld(btVector3(-1.3f, 0.f, -1.1f), btVector3(0.1f, 6.f, 3.2f));
-		//start.attachToWorld(btVector3(90.0f, 8.f, 110.0f), btVector3(3.f, 3.f, 3.f));
+		finish.attachToWorld(btVector3(-1.3f, 0.f, -1.1f), btVector3(0.1f, 6.f, 3.2f));
+		start.attachToWorld(btVector3(90.0f, 8.f, 110.0f), btVector3(3.f, 3.f, 3.f));
         finishTimer.reset();
     }
 
@@ -31,12 +31,12 @@ namespace Rally { namespace Model {
     }
 
 	void World::printFinishedTime(){
-		/*if(!start.hasCollided())
+		if(!start.hasCollided())
 			finishTimer.reset();
 
 		if(start.hasCollided() && finish.hasCollided() && finish.isEnabled()){
 			finish.setEnabled(false);
 			std::cout << "Finished after: " << finishTimer.getElapsedSeconds() << " seconds!" << std::endl;
-		}*/
+		}
 	}
 } }

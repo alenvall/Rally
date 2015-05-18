a54a32083eab8305b2bba4ba26994877c33174ac
Joel Severin <git@joelhemsida.se>
2015-05-18 23:32:22 +0200

    Revert "Using dFdx/dFdy for finding normal in SSAO instead of using a gbuffer."
    This reverts commit 58e97d3c482658d210712283a88aba41d816aea8.

--- dist/media/compositors/render_gbuffer_vs.glsl
+++ dist/media/compositors/render_gbuffer_vs.glsl
@@ -6,11 +6,11 @@ uniform mat4 worldViewProj;
 uniform mat4 worldView;
 
 varying vec3 viewSpacePosition;
-//varying vec3 viewSpaceNormal;
+varying vec3 viewSpaceNormal;
 
 void main() {
     viewSpacePosition = (worldView*vec4(gl_Vertex.xyz/gl_Vertex.w, 1.0)).xyz;
-    //viewSpaceNormal = (worldView * vec4(gl_Normal, 0.0)).xyz;
+    viewSpaceNormal = (worldView * vec4(gl_Normal, 0.0)).xyz;
     //viewSpaceNormal = (mat3(transpose(inverse(worldView))) * gl_Normal).xyz;
     
     gl_Position = worldViewProj * gl_Vertex;
